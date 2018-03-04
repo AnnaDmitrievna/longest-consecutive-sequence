69bf2a053f33645c1caa33087803f56e098aa2d2
Anna <annadmitrievna1823@gmail.com>
2018-03-04 21:17:50 +0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
-}
+
+    let compareFunction = (a, b) => a - b;
+    let sortArray = array.sort(compareFunction);
+    let count = 1;
+    let length = 1;
+
+    if (sortArray.length <= 1) {
+        return sortArray.length;
+    }
+
+    for (let i = 0; i < sortArray.length; i++) {
+        if (sortArray[i] + 1 === sortArray[i + 1]) {
+            count++;
+
+            if (count > length) {
+                length = count;
+            }
+
+        } else if (sortArray[i] !== sortArray[i + 1]) {
+            count = 1;
+        }
+    }
+
+    return length;
+};
+
